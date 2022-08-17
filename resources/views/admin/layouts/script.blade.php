3d878d844e36b3cea3953b65963a4ba38dd85fa7
Hanieh Rohban <h.rohban@gmail.com>
2022-08-17 18:43:35 +0100

    firstCommit_html_ux

---
+++ resources/views/admin/layouts/script.blade.php
@@ -0,0 +1,12 @@
+
+    <script src="{{ asset('admin-assets/js/jquery-3.5.1.min.js') }}"></script>
+    <script src="{{ asset('admin-assets/js/popper.js') }}" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+   
+
+    <script src="{{ asset('admin-assets/js/bootstrap/bootstrap.js') }}"></script>
+    <script src="{{ asset('admin-assets/js/grid.js') }}"></script>
+
+
+
+
+    
